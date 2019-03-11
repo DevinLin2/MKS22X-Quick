0df6d4317ceec7fe87aa6980f3b0f91afe541ae4
VitaminOh <dlin01@stuy.edu>
2019-03-11 10:15:51 -0400

    skeleton of functions

---
+++ Quick.java
@@ -0,0 +1,10 @@
+public class Quick {
+
+  private static int partition() {
+    
+  }
+  // return the value that is the kth smallest value of the array.
+  public static int quickselect(int[] data, int k) {
+
+  }
+}
