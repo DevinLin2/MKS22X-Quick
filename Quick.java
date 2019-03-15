bd6d38a303ee200dbaa45fafdddc66ca00f64754
Devin Lin <Dlin01@stuy.edu>
2019-03-15 07:42:05 -0400

    changed my linear pass to insert pivot into the ary as that was taking too long; changed it to just throw pivot into the element before or after the final start index

--- Quick.java
+++ Quick.java
@@ -4,23 +4,39 @@ public class Quick {
 
   private static Random randgen = new Random();
 
-  public static void main(String[] args) {
-    int[] data = new int[30];
-    for (int i = 0; i < data.length; i++) {
-      data[i] = Math.abs(randgen.nextInt() % 100);
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Quick.quicksort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
     }
-    // System.out.println(partition(data, 0, data.length -1));
-    // System.out.println(Arrays.toString(data));
-    // System.out.println(quickselect(data, 0));
-    // System.out.println(quickselect(data, 1));
-    // System.out.println(quickselect(data, 2));
-    // System.out.println(quickselect(data, 3));
-    // System.out.println(quickselect(data, 4));
-    // System.out.println(quickselect(data, 5));
-    quicksort(data);
-    // System.out.println(partition(data, 0, data.length-1));
-    System.out.println(Arrays.toString(data));
-    System.out.println(isSorted(data));
   }
   /*Modify the array such that:
   *1. Only the indices from start to end inclusive are considered in range
@@ -32,49 +48,85 @@ public class Quick {
   */
   private static int partition (int[] data, int start, int end) {
     int pivotIndex = Math.abs(randgen.nextInt() % (end - start + 1)) + start;
+    if (start - end > 3) {
+      pivotIndex = median(data, start, end);
+    }
     int pivot = data[pivotIndex];
     int temp = 0;
-    int s = start;
-    int e = end;
+    int startIndex = start;
     // System.out.println("pivot: " + pivot);
     // swap start and the pivot if they are different index else move start up one
-    if (pivotIndex != s) {
-      data[pivotIndex] = data[s];
-      data[s] = pivot;
-      s ++;
-    } else {
-      s ++;
-    }
-    while (s != e) {
-      if (data[s] > pivot && s != e) {
+    data[pivotIndex] = data[start];
+    data[start] = pivot;
+    start ++;
+    while (start < end) {
+      if (data[start] > pivot) {
         //System.out.println("top");
         // System.out.println("S: " + s);
         // System.out.println("E: " + e);
-        temp = data[s];
-        data[s] = data[e];
-        data[e] = temp;
-        e--;
+        temp = data[end];
+        data[end] = data[start];
+        data[start] = temp;
+        end--;
         //System.out.println(Arrays.toString(data));
       }
-      if (data[s] <= pivot && s != e) {
+      if (data[start] < pivot) {
         //System.out.println("bottom");
-        s++;
+        start++;
+      }
+      if (data[start] == pivot) {
+        int randInt = Math.abs(randgen.nextInt()) % 2;
+        if (randInt == 0) {
+          temp = data[end];
+          data[end] = data[start];
+          data[start] = temp;
+          end--;
+        }
+        if (randInt == 1) {
+          start ++;
+        }
       }
     }
-    for (int i = start+1; i <= end; i++) {
-      if (pivot < data[i]) {
-        pivotIndex = i - 1;
-        temp = data[pivotIndex];
-        data[pivotIndex] = pivot;
-        data[start] = temp;
-        return pivotIndex;
-      }
-    }// if this loop runs without returning anything then the pivot is the largest number
-    temp = data[end];
-    pivotIndex = end;
-    data[end] = pivot;
-    data[start] = temp;
-    return pivotIndex;
+    // for (int i = start+1; i <= end; i++) {
+    //   if (pivot < data[i]) {
+    //     pivotIndex = i - 1;
+    //     temp = data[pivotIndex];
+    //     data[pivotIndex] = pivot;
+    //     data[start] = temp;
+    //     return pivotIndex;
+    //   }
+    // }// if this loop runs without returning anything then the pivot is the largest number
+    // temp = data[end];
+    // pivotIndex = end;
+    // data[end] = pivot;
+    // data[start] = temp;
+    // return pivotIndex;
+    if (data[start] < pivot) {
+      data[startIndex] = data[start];
+      data[start] = pivot;
+      return start;
+    }
+    if (data[start] > pivot) {
+      data[startIndex] = data[start - 1];
+      data[start - 1] = pivot;
+      return start - 1;
+    }
+    return start;
+  }
+  private static int median(int[] data, int start, int end) {
+    int startNum = data[start];
+    int endNum = data[end];
+    int midNum = data[data.length/2];
+    if ((startNum < endNum && startNum > midNum) || (startNum > endNum && startNum < midNum)) {
+      return start;
+    }
+    if ((endNum < startNum && endNum > midNum) || (endNum > startNum && endNum < midNum)) {
+      return end;
+    }
+    if ((midNum < endNum && midNum > startNum) || (midNum > endNum && midNum < startNum)) {
+      return data.length/2;
+    }
+    return -1;
   }
   // return the value that is the kth smallest value of the array.
   public static int quickselect(int[] data, int k) {
