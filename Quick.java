bb2a1b277cb4479a1f8813214ee993cf1d7d0327
VitaminOh <dlin01@stuy.edu>
2019-03-19 10:16:56 -0400

    added insertion

--- Quick.java
+++ Quick.java
@@ -5,38 +5,41 @@ public class Quick {
   private static Random randgen = new Random();
 
   public static void main(String[]args){
-    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-    int[]MAX_LIST = {1000000000,500,10};
-    for(int MAX : MAX_LIST){
-      for(int size = 31250; size < 2000001; size*=2){
-        long qtime=0;
-        long btime=0;
-        //average of 5 sorts.
-        for(int trial = 0 ; trial <=5; trial++){
-          int []data1 = new int[size];
-          int []data2 = new int[size];
-          for(int i = 0; i < data1.length; i++){
-            data1[i] = (int)(Math.random()*MAX);
-            data2[i] = data1[i];
-          }
-          long t1,t2;
-          t1 = System.currentTimeMillis();
-          Quick.quicksort(data2);
-          t2 = System.currentTimeMillis();
-          qtime += t2 - t1;
-          t1 = System.currentTimeMillis();
-          Arrays.sort(data1);
-          t2 = System.currentTimeMillis();
-          btime+= t2 - t1;
-          if(!Arrays.equals(data1,data2)){
-            System.out.println("FAIL TO SORT!");
-            System.exit(0);
-          }
-        }
-        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-      }
-      System.out.println();
-    }
+    // System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    // int[]MAX_LIST = {1000000000,500,10};
+    // for(int MAX : MAX_LIST){
+    //   for(int size = 31250; size < 2000001; size*=2){
+    //     long qtime=0;
+    //     long btime=0;
+    //     //average of 5 sorts.
+    //     for(int trial = 0 ; trial <=5; trial++){
+    //       int []data1 = new int[size];
+    //       int []data2 = new int[size];
+    //       for(int i = 0; i < data1.length; i++){
+    //         data1[i] = (int)(Math.random()*MAX);
+    //         data2[i] = data1[i];
+    //       }
+    //       long t1,t2;
+    //       t1 = System.currentTimeMillis();
+    //       Quick.quicksort(data2);
+    //       t2 = System.currentTimeMillis();
+    //       qtime += t2 - t1;
+    //       t1 = System.currentTimeMillis();
+    //       Arrays.sort(data1);
+    //       t2 = System.currentTimeMillis();
+    //       btime+= t2 - t1;
+    //       if(!Arrays.equals(data1,data2)){
+    //         System.out.println("FAIL TO SORT!");
+    //         System.exit(0);
+    //       }
+    //     }
+    //     System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    //   }
+    //   System.out.println();
+    // }
+    int[] data = new int[]{123, 1234,123, 435, 234, 56, 23, 5, 245};
+    insertionSort(data, 0, data.length - 1);
+    System.out.println(Arrays.toString(data));
   }
   /*Modify the array such that:
   *1. Only the indices from start to end inclusive are considered in range
@@ -170,5 +173,18 @@ public class Quick {
       }
     }
     return true;
+  }
+
+  private static void insertionSort(int[] data, int start, int end) {
+    for (int i = start + 1; i <= end; i++){
+      int storage = data[i];
+      int index = i - 1;
+      while(index >= start && storage < data[index]){
+        data[i] = data[index]; //this is just changing data[i] fix this
+        index--;
+      }
+      data[index] = storage;
+      index = i - 1;
+    }
   }
 }
